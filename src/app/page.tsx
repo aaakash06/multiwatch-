830d82e0d7a98dfad483e47e3560a7004aca5273
aaakash06 <akashbagale182@gmail.com>
2024-11-20 15:41:27 +0545

    main component add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "@/components/NavBar";
-import WalletGenerator from "@/components/WalletGenerator";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh]">
       <Navbar />
-      <WalletGenerator />
+      <Main />
     </main>
   );
 }
