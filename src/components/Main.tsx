4e10e656fcd61e337af56e0b96c387cbff049286
aaakash06 <akashbagale182@gmail.com>
2024-11-20 15:48:25 +0545

    sonner add

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,8 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-import nacl from "tweetnacl";
-import { Input } from "./ui/input";
-import { easeIn, easeInOut, motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { motion } from "framer-motion";
 import {
   ChevronDown,
   ChevronUp,
