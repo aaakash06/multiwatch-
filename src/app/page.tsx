fca6ac67ca9d28b3a1f2efc751bc67a570ed316c
aaakash06 <akashbagale182@gmail.com>
2024-11-20 15:33:55 +0545

    page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import HomeScreen from "@/components/Home4";
 import React from "react";
 
 export default function Home() {
-  return <HomeScreen />;
+  return (
+    <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh]">
+      <Navbar />
+      <WalletGenerator />
+    </main>
+  );
 }
