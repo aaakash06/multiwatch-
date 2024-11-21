41cec054102d611ee2fb4a588cc55d09b1c3a785
aaakash06 <akashbagale182@gmail.com>
2024-11-21 15:45:06 +0545

    store fix

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,13 +1,18 @@
-import { create } from "domain";
 import { Clock } from "./types";
+import { persist } from "zustand/middleware";
+import { StateCreator } from "zustand";
+import { create } from "zustand";
 interface ClockStore {
   clocks: Clock[];
   addClock: (clock: Clock) => void;
 }
 
-const useClockStore = create<ClockStore>((set) => ({
+const storeObject: StateCreator<ClockStore> = (set, get) => ({
   clocks: [],
-  addClock: (clock) => set((state) => ({ clocks: [...state.clocks, clock] })),
-}));
+  addClock: (clock: Clock) =>
+    set((state) => ({ clocks: [...state.clocks, clock] })),
+});
+
+const useClockStore = create(persist(storeObject, { name: "clock-store" }));
 
 export default useClockStore;
